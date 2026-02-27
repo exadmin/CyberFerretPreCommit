5b8c2f267be9650282df5d75df2b9dd8841c3fbb
exadmin <smirnovi.qubership@gmail.com>
2026-02-27 20:51:12 +0300

    feat: pre-commit hooks, version 1.0.0

---
+++ call-cyberferret.cmd
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+if "%CYBER_FERRET_PATH%"=="" (
+  echo 'CYBER_FERRET_PATH' System Variable is not set. 1>&2
+  echo It must point to path of 'https://github.com/exadmin/CyberFerret' repository clone
+  exit /b 1
+)
+
+if not exist "%CYBER_FERRET_PATH%/cyberferret.jar" (
+  echo File '%CYBER_FERRET_PATH%/cyberferret.jar' does not exist. 1>&2
+  echo "Also, compile & build project before use, probably run 'mvn clean package assembly:single' but better see README.md" 1>&2
+  exit /b 1
+)
+
+call javaw -cp %CYBER_FERRET_PATH%/cyberferret.jar com.github.exadmin.cyberferret.CyberFerretCLI %CD%
+
